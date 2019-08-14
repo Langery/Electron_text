2114c6a7c734805a50e71bf602148936c47fcd34
Langery <1245066197@qq.com>
2019-08-14 22:17:47 +0800

    finished communication with Flask

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,6 +2,16 @@ const electron = require('electron')
 const ipc = electron.ipcRenderer
 const loginBtn = document.getElementById('loginBtn')
 
+var ajax = new XMLHttpRequest()
+
+
 loginBtn.addEventListener('click', () => {
-  ipc.send('login-click')
+  ajax.open('GET', 'http://127.0.0.1:5000/first', true)
+  ajax.send()
+  ajax.onreadystatechange = () => {
+    if (ajax.readyState == 4 && ajax.status == 200) {
+      console.log(ajax.responseText)
+      ipc.send('login-click')
+    }
+  }
 })
