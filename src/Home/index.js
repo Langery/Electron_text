966690689e5bfe0c833ebc9b9658a52fc0dcbb21
Langery <1245066197@qq.com>
2019-08-16 14:47:52 +0800

    merge

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,39 +1,21 @@
 const electron = require('electron')
 const ipc = electron.ipcRenderer
-const loginBtn = document.getElementById('loginBtn')
-// var loginBtn = element.getElementById('loginBtn')
-const registerBtn = document.getElementById('registerBtn')
-// var registerBtn = element.getElementById('registerBtn')
+var loginBtn = document.getElementById('loginBtn')
+var registerBtn = document.getElementById('registerBtn')
 
 var ajax = new XMLHttpRequest()
 
 loginBtn.onclick = () => {
-  ajax.open('GET', 'http://127.0.0.1:5000/first', true)
-  ajax.send()
-  ajax.onreadystatechange = () => {
-    if (ajax.readyState == 4 && ajax.status == 200) {
-      console.log(ajax.responseText)
+  // ajax.open('GET', 'http://127.0.0.1:5000/first', true)
+  // ajax.send()
+  // ajax.onreadystatechange = () => {
+  //   if (ajax.readyState == 4 && ajax.status == 200) {
+  //     console.log(ajax.responseText)
       ipc.send('login-click')
-    }
-  }
+  //   }
+  // }
 }
 
 registerBtn.onclick = () => {
   ipc.send('register-click')
 }
-
-
-// function loginBtn () {
-//   ajax.open('GET', 'http://127.0.0.1:5000/first', true)
-//   ajax.send()
-//   ajax.onreadystatechange = () => {
-//     if (ajax.readyState == 4 && ajax.status == 200) {
-//       console.log(ajax.responseText)
-//       ipc.send('login-click')
-//     }
-//   }
-// }
-
-// function registerBtn () {
-//   ipc.send('register-click')
-// }
