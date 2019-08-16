d3b73f05193ccfebc809120e247d84f81fe23789
Langery <1245066197@qq.com>
2019-08-16 15:18:12 +0800

    finished the page jump

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,21 +1,26 @@
 const electron = require('electron')
 const ipc = electron.ipcRenderer
-var loginBtn = document.getElementById('loginBtn')
-var registerBtn = document.getElementById('registerBtn')
+// var loginBtn = document.getElementById('loginBtn')
+// var registerBtn = document.getElementById('registerBtn')
 
 var ajax = new XMLHttpRequest()
 
-loginBtn.onclick = () => {
-  // ajax.open('GET', 'http://127.0.0.1:5000/first', true)
-  // ajax.send()
-  // ajax.onreadystatechange = () => {
-  //   if (ajax.readyState == 4 && ajax.status == 200) {
-  //     console.log(ajax.responseText)
-      ipc.send('login-click')
-  //   }
-  // }
+function loginFun (target) {
+  console.log('++++++++')
+  target.onclick = () => {
+    console.log('--------------')
+    // ajax.open('GET', 'http://127.0.0.1:5000/first', true)
+    // ajax.send()
+    // ajax.onreadystatechange = () => {
+    //   if (ajax.readyState == 4 && ajax.status == 200) {
+    //     console.log(ajax.responseText)
+        ipc.send('login-click')
+    //   }
+    // }
+  }
 }
-
-registerBtn.onclick = () => {
-  ipc.send('register-click')
+function registerFun (target) {
+  target.onclick = () => {
+    ipc.send('register-click')
+  }
 }
