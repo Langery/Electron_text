5f76fd7c66118d9339c82e39d750350eee8e9eba
Langery <1245066197@qq.com>
2019-08-17 13:14:58 +0800

    get send info

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,30 +1,30 @@
 const electron = require('electron')
 const ipc = electron.ipcRenderer
-// var loginBtn = document.getElementById('loginBtn')
-// var registerBtn = document.getElementById('registerBtn')
 
 var ajax = new XMLHttpRequest()
 
 function loginFun (target) {
-  console.log('++++++++')
   target.onclick = () => {
-    console.log('--------------')
-    ajax.open('GET', 'http://127.0.0.1:5000/first', true)
+    var sendData = 'langery'
+    ajax.open('GET', 'http://127.0.0.1:5000/first?name=' + sendData, true)
     ajax.send()
     ajax.onreadystatechange = () => {
       if (ajax.readyState == 4 && ajax.status == 200) {
-        console.log(ajax.responseText)
+        sessionStorage.setItem('backInfo', ajax.responseText)
         ipc.send('login-click')
       }
     }
   }
 }
+
 function registerFun (target) {
   target.onclick = () => { ipc.send('register-click') }
 }
 
 function backFun (target) {
-  target.onclick = () => {
-    ipc.send('back-click')
-  }
+  target.onclick = () => { ipc.send('back-click') }
+}
+
+function sendIPC (target, sendAim) {
+  target.onclick = () => { ipc.send(sendAim) }
 }
