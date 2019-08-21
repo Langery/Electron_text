0e847c5543e51f5c0a4eb87d1ce5ae2823c0b511
Langery <1245066197@qq.com>
2019-08-21 22:59:31 +0800

    add post

--- src/Home/index.js
+++ src/Home/index.js
@@ -26,8 +26,20 @@ function onLogin (target, name, pwd) {
     var username = name.value
     var password = pwd.value
     // console.log(username, password)
-    ajax.open('GET', 'http://127.0.0.1:5000/login?username=' + username + '&password=' + password)
-    ajax.send()
+    // ajax.open('GET', 'http://127.0.0.1:5000/login?username=' + username + '&password=' + password, true)
+    // ajax.send()
+    // ajax.onreadystatechange = () => {
+    //   if (ajax.readyState == 4 && ajax.status == 200) {
+    //     console.log(ajax.responseText)
+    //   }
+    // }
+    // var sendPost = {
+    //   name: name.value,
+    //   password: pwd.value
+    // }
+    ajax.open('POST', 'http://127.0.0.1:5000/login', true)
+    ajax.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+    ajax.send('name=' + name.value + '&password=' + pwd.value)
     ajax.onreadystatechange = () => {
       if (ajax.readyState == 4 && ajax.status == 200) {
         console.log(ajax.responseText)
