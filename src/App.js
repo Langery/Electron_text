ae0d30244423c3f8f1e0289f29f9e4ba177c0b0a
Langery <1245066197@qq.com>
2019-09-05 21:42:15 +0800

    merge

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import HelloIndex from './view/Hello/hello'
 
 class App extends Component {
   render () {
     return (
       <Router>
-        <Switch>
-          <div className="main-style">
-            <HelloIndex></HelloIndex>
-          </div>
-        </Switch>
+        <div className="main-style">
+          <HelloIndex></HelloIndex>
+        </div>
       </Router>
     )
   }
