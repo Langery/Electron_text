7b0d3f626891653bb6e50ff2b1a9ac8b12fdf3b9
Langery <1245066197@qq.com>
2022-11-01 10:18:33 +0800

    merge

--- src/view/components/excel.js
+++ src/view/components/excel.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import '../../style/excel.less';
+
 import { UploadOutlined  } from '@ant-design/icons';
 
 import { Button, Upload } from 'antd'; // message
@@ -7,12 +9,15 @@ import { Button, Upload } from 'antd'; // message
 const ExcelSelf = React.forwardRef((props, ref) => {
 
   const ExcelChangeValue = info => {
+    console.log('=============================>');
     console.log(info)
+    const file = info.file;
+    console.log(file);
   }
 
   return (
     <div className="main_excel">
-      <Upload onChange={ExcelChangeValue}>
+      <Upload action="" onChange={ExcelChangeValue}>
         <Button icon={<UploadOutlined />}>Click to Upload</Button>
       </Upload>
     </div>
